26853b04cef521548fa6d30f84afb9839307af20
Jesus Lencina <jesusignaciolencina@gmail.com>
2022-06-25 19:19:10 -0300

    added link to repo on footer

--- src/pages/common-components/footer/Footer.jsx
+++ src/pages/common-components/footer/Footer.jsx
@@ -4,6 +4,12 @@ const Footer = () => (
     <footer>
         <p>Copyright © 2022 VinylArtCba</p>
         <p>Todos los derechos reservados</p>
+        <a
+            href="https://github.com/jesuslencina/vinylartcba-web"
+            target="_blank"
+            className="btn light">
+            Ver en Github
+        </a>
     </footer>
 );
 
